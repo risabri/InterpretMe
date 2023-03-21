533ad37255b4b6fcdbf03b9751774f6af47adde7
Rita Sabri <rsabri@seas.upenn.edu>
2023-03-21 10:01:00 -0400

    create accounr
    account

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Loginpage from './pages/Loginpage';
+import LoginButton from './pages/Loginbutton';
+import Profile from './pages/Profile';
+import LogoutButton from './pages/Logoutbutton';
 import Demographic from './pages/Demographic';
 import Explanation from './pages/Explanation';
 import Detailpage from './pages/Detailpage';
-import Upload from './pages/Upload';
-import Upload_2 from './pages/Upload_2';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 
 function App() {
+  const { isLoading, error } = useAuth0();
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<div>
+          <h1>
+            Auth0 log in </h1>
+            <LoginButton/>
+            {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+            <Profile/>
+            <LogoutButton/>
+            </>
+          )}
+        
+        </div>} />
         <Route path="/demographic" element={<Demographic />} />
         <Route path="/loginpage" element={<Loginpage />} />
         <Route path="/explanation" element={<Explanation />} />
 
         <Route path="/detailpage" element={<Detailpage/>} />
 
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/upload2" element={<Upload_2 />} />
       </Routes>
     </Router>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-udkffa48qzrzmfij.us.auth0.com"
+    clientId="0741SZDMdOhyvBfKUqnWasGl630iarqq"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
+
 
