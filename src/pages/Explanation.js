36579d921079b6b93835b3f9cdf0c5a746a7fbb6
Rita Sabri <92890709+risabri@users.noreply.github.com>
2023-03-28 17:03:37 -0400

    Update Explanation.js

--- src/pages/Explanation.js
+++ src/pages/Explanation.js
@@ -44,7 +44,7 @@ const Explanation = ({ onDetailpage }) => {
 
   return (
     <div className="logoContainer ">
-      {imageURL && <img src={imageURL} alt="uploaded" className="uploaded-image" />}
+      <img src={imageURL || logo1} alt="logo1" className={imageURL ? "uploaded-image" : "logo"} />
       <div className="container">
         <h1 className="title"> </h1>
         <form onSubmit={handleSubmit}>
