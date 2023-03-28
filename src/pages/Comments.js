fec83a61abfd698daac62f7db503c88459c86e2c
Rita Sabri <rsabri@seas.upenn.edu>
2023-03-28 12:53:52 -0400

    Fixed file name capitalization

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -1,1 +1,5 @@
 // add display of the uploaded social media post.
+// add previous button 
+// make video run through the flow ( less than one minute )
+// replace the interpret me logo with the uploaded post 
+//
